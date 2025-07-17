992e30a7aaae118532d18cf207d455a16109fbc1
Ilham Maulana <ilhammln1510@gmail.com>
2025-07-17 18:53:53 +0700

    update db railway

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
+import os
 import mysql.connector
 
 def get_db_connection():
     return mysql.connector.connect(
-        host='localhost',
-        user='dfaal496_gesture_transfer',
-        password='gesture_transfer1101D',  # Ganti dengan password MySQL kamu
-        database='dfaal496_gesture_transfer'
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DB_NAME')
     )
+
